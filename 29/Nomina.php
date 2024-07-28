e3f6d0b5e3fdb3b094e3ca60b8166d284167041c
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-07-28 09:59:20 -0500

    Add files via upload

---
+++ 29/Nomina.php
@@ -0,0 +1,16 @@
+﻿<?php
+//Importa la librería genérica para bases de datos
+require_once("BD.php");
+
+//Llama la parte visual
+$plantilla = file_get_contents('Nomina.html');
+$texto = ""; //Lo que reemplazará en la plantilla
+
+//Instancia la librería genérica y hace uso de los métodos
+$BD = new basedatos();
+$numRegistros = $BD->TotalRegistros('Nomina'); //Prepara la consulta
+$texto = "<tr><td>Nomina</td><td>" . $numRegistros . "</td></tr>";	
+
+//Modifica el HTML (parte visual) y lo muestra
+$resultado = str_replace('{Datos}', $texto, $plantilla);
+echo $resultado;
