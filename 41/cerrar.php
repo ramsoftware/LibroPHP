74980e5986dfd6a47dbb4e7d92795c9957f9f122
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-07-28 10:02:10 -0500

    Add files via upload

---
+++ 41/cerrar.php
@@ -0,0 +1,7 @@
+﻿<?php
+session_start();
+
+//Destruye las variables y redirige a la pantalla de inicio
+unset ($_SESSION['permitido']);
+session_destroy();		
+header("Location: sesion.html");
---
+++ 41/importante.php
@@ -0,0 +1,20 @@
+﻿<?php
+//Llama la parte visual
+$plantilla = file_get_contents('importante.html');
+
+//Lee una variable de sesión
+session_start();
+
+//Con isset se determina si la variable de sesión ha sido creada
+if (isset($_SESSION['permitido'])){
+	if ($_SESSION['permitido'] == 'Todo bien')
+		$texto = str_replace('{informacion}', "Información importante", $plantilla);	
+	else
+		$texto = str_replace('{informacion}', "ACCESO DENEGADO", $plantilla);
+}
+else {
+	$texto = str_replace('{informacion}', "ACCESO DENEGADO", $plantilla);
+}
+
+//Muestra la página
+echo $texto;
