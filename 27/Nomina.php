4d31c4e3db683b0145f86f0222240a1902c84ada
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-07-28 09:58:54 -0500

    Add files via upload

---
+++ 27/Nomina.php
@@ -0,0 +1,31 @@
+﻿<?php
+//Importa la librería genérica para bases de datos
+require_once("BD.php");
+
+//Llama la parte visual
+$plantilla = file_get_contents('Nomina.html');
+$texto = ""; //Lo que reemplazará en la plantilla
+
+//Instancia la librería genérica y hace uso de los métodos
+$BD = new basedatos();
+$BD->PrepararLectura('SELECT nomina.nombre1, nomina.nombre2, nomina.apellido1, nomina.apellido2, ciudad.nombre FROM nomina, ciudad WHERE nomina.ciudad = ciudad.codigo ORDER BY nomina.nombre1, nomina.nombre2, nomina.apellido1, nomina.apellido2'); //Prepara la consulta	
+while(($registros = $BD->TraeRegistroARegistro()) != null){
+	$nombre1 = $registros[0];
+	$nombre2 = $registros[1];
+	$apellido1 = $registros[2];
+	$apellido2 = $registros[3];
+	$ciudad = $registros[4];
+	
+	//htmlentities para evitar ejecutar código HTML
+	$texto .= "<tr>";
+	$texto .= "<td>" . htmlentities($nombre1, ENT_QUOTES, "UTF-8") . "</td>";
+	$texto .= "<td>" . htmlentities($nombre2, ENT_QUOTES, "UTF-8") . "</td>";
+	$texto .= "<td>" . htmlentities($apellido1, ENT_QUOTES, "UTF-8") . "</td>";
+	$texto .= "<td>" . htmlentities($apellido2, ENT_QUOTES, "UTF-8") . "</td>";
+	$texto .= "<td>" . htmlentities($ciudad, ENT_QUOTES, "UTF-8") . "</td>";
+	$texto .= "<tr>";
+}
+
+//Modifica el HTML (parte visual) y lo muestra
+$resultado = str_replace('{Datos}', $texto, $plantilla);
+echo $resultado;
---
+++ 27/script1.sql
@@ -0,0 +1,3 @@
+﻿SELECT nombre1, nombre2, apellido1, apellido2, ciudad
+FROM nomina
+ORDER BY nombre1, nombre2, apellido1, apellido2
