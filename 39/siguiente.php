d9d27309d3605f9c57f4cd03551d4d9226e5cb90
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2024-07-28 10:01:43 -0500

    Add files via upload

---
+++ 39/siguiente.php
@@ -0,0 +1,16 @@
+﻿<?php
+//Lee una variable de sesión
+session_start();
+
+//Con isset se determina si la variable de sesión ha sido creada
+if (isset($_SESSION['mantener'])){
+	echo "variable definida es: " . $_SESSION['mantener'];
+	
+	//Destruye la variable y cierra la sesión	
+	unset($_SESSION['mantener']);
+	session_destroy();
+	echo "<br><br>sesión cerrada";
+}
+else {
+	echo "sesión NO iniciada";
+}
